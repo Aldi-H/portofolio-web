64728c0f6802c9f0d48952f4c4507c6df59479e6
unknown <aldihimawan88@gmail.com>
2022-01-04 11:07:52 +0700

    adding projects.js components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import profileImage from "../images/image.png";
 import codingImage from "../images/Programming-rafiki.png";
 
 const Home = () => {
--- src/data.js
+++ src/data.js
@@ -7,3 +7,22 @@ export const skills = [
   "JavaScript",
   "Node.JS",
 ];
+
+export const projects = [
+  {
+    title: "ToDo List App",
+    subtitle: "EJS and Express JS",
+    description:
+      "ToDo List app menggunakan express.js dan EJS dan server client menggunakan Node.JS dengan tujuan mempelajari EJS dan express.js",
+    image: require("./images/toDoListApp.png"),
+    repositories: "https://github.com/Aldi-H/todoList-V1",
+  },
+  {
+    title: "Portofolio Web",
+    subtitle: "React and Tailwindcss",
+    description:
+      "Portofolio Web menggunakan react dan tailwindcss sebagai style dengan tujuan mempelajari React dan membuat suatu website portofolio",
+    image: require("./images/portofolioWeb.png"),
+    repositories: "https://github.com/Aldi-H/portofolio-web",
+  },
+];
