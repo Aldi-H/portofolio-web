e388fa2a8b24de8e95a035fa3eb05c4ab389aa5b
unknown <aldihimawan88@gmail.com>
2022-01-04 08:50:37 +0700

    modified navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,135 +1,37 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { Transition } from "@headlessui/react";
-import profileImage from "../images/image.png";
 
-function classNames(...classes) {
+/* function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
-}
+} */
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  /* const [isOpen, setIsOpen] = useState(false); */
   return (
-    <div>
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <div className="title-font font-medium text-white md:mb-0">
-                  <span className="block lg:block h-8 w-auto uppercase ml-3 text-2xl fontFamily:roboto">
-                    Aldi Himawan
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <Link
-                  to="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
-                >
-                  Home
-                </Link>
-
-                <Link
-                  to="/projects"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
-                >
-                  Projects
-                </Link>
-
-                <Link
-                  to="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
-                >
-                  Contact
-                </Link>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#" className="ml-3 text-xl uppercase">
+            Aldi Himawan
+          </a>
+        </a>
+        <nav className="flex flex-wrap items-center text-base justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Projects
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <Link
-                  to="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Home
-                </Link>
-
-                <Link
-                  to="/projects"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Projects
-                </Link>
-
-                <Link
-                  to="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Contacts
-                </Link>
-              </div>
-            </div>
-          )}
-        </Transition>
-      </nav>
-    </div>
+          Contact Me
+        </a>
+      </div>
+    </header>
   );
 };
 
